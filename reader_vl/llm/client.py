b478bf4acc7e031deb79ac75d6b05030bc8e1f4e
Aquos06 <ruliciohansen@gmail.com>
2025-02-03 23:07:01 +0800

    fix(llm-client-base): add stream function

--- reader_vl/llm/client.py
+++ reader_vl/llm/client.py
@@ -1,11 +1,13 @@
+from typing import List, Optional, Generator
 from abc import ABC, abstractmethod
-from typing import List
 
 from llm.schemas import CompletionResponse, ChatMessage, ChatCompletionResponse
 
 class llmBase(ABC):
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, model: str, max_tokens: Optional[int]) -> None:
         self.url = url
+        self.model = model
+        self.max_tokens = max_tokens
 
     @abstractmethod
     def completion(self, prompt: str, *args, **kwargs) -> CompletionResponse:
@@ -22,3 +24,16 @@ class llmBase(ABC):
     @abstractmethod
     def achat(self, message: List[ChatMessage], *args, **kwargs) -> ChatCompletionResponse:
         pass
+    
+    @abstractmethod
+    def completion_stream(self, prompt: str, *args, **kwargs) -> Generator[CompletionResponse]:
+        raise ValueError("streaming is not supported")
+    
+    def acompletion_stream(self, prompt: str, *args, **kwargs) -> Generator[CompletionResponse]:
+        raise ValueError("streaming is not supported")
+    
+    def chat_stream(self, message: List[ChatMessage], *args, **kwargs) -> Generator[ChatCompletionResponse]:
+        raise ValueError("streaming is not supported")
+    
+    def achat_stream(self, message: List[ChatMessage], *args, **kwargs) -> Generator[ChatCompletionResponse]:
+        raise ValueError("streaming is not supported")
