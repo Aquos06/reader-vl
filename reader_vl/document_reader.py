0268edda3dd7fff75ef4637bd4698e3824923a5d
Aquos06 <ruliciohansen@gmail.com>
2025-02-19 00:05:43 +0800

    feat(export): export to json and markdown first version

--- reader_vl/document_reader.py
+++ reader_vl/document_reader.py
@@ -1,4 +1,5 @@
 import cv2
+import base64
 import logging
 import numpy as np
 from tqdm import tqdm
@@ -9,6 +10,7 @@ from reader_vl.docs.yolo import YOLO
 from reader_vl.llm.client import llmBase
 from reader_vl.docs.utils import open_file, pdf2image
 from reader_vl.docs.structure.core import StructureBase
+from reader_vl.docs.structure.schemas import ContentType
 from reader_vl.docs.schemas import Document, Page, Component
 from reader_vl.docs.structure.registry import CLASS_REGISTRY
 from reader_vl.models.utils import get_models_path
@@ -24,7 +26,8 @@ class DocReader:
             metadata: Optional[dict] = None,
             yolo_parameters: Optional[dict] = {},
             verbose: Optional[bool] = True,
-            failed_image_path: Optional[Union[str, Path]] = Path("./failed.jpg")
+            failed_image_path: Optional[Union[str, Path]] = Path("./failed.jpg"),
+            auto_parse: Optional[bool] = True,
         )->None:
         self.check_arguments(file_path, file_bytes)
 
@@ -45,6 +48,11 @@ class DocReader:
         self.file_name = file_path.name if file_path else None
         self.file_path = file_path
         self.file_bytes = file_bytes
+        
+        self.parsed_document = None
+        if auto_parse:
+            self.parsed_document:Document = self.parse()
+        
         
     def check_arguments(file_path: Optional[Union[Path, str]], file_bytes: bytes) -> None:
         if file_bytes and file_path:
@@ -113,3 +121,82 @@ class DocReader:
     async def aparse(self)->Document:
         images = pdf2image(self.file_bytes)
         return self._parse(images=images)
+    
+    def export_to_json(self) -> List[dict]:
+        if self.parsed_document == None:
+            self.parsed_document = self.parse()
+
+        json_list: List[dict] = []
+        for page in self.parsed_document.page:
+            for component in page.component:
+                json_list.append(component.model_dump_json())
+
+        return json_list
+    
+    def export_to_markdown(self, ignore_footer: Optional[bool] = True) -> str:
+        if self.parsed_document == None:
+            self.parsed_document = self.parse()
+
+        markdown_output = "" 
+        for page in self.parsed_document.page:
+            for component in page.component:
+                if component.component_type == ContentType.HEADER:
+                    markdown_output += f"### {component.content}\n\n"
+                elif component.component_type == ContentType.SECTION:
+                    markdown_output += f"{component.content} \n"
+                elif component.component_type == ContentType.TABLE:
+                    markdown_output += self._format_table(component.content)
+                elif component.component_type == ContentType.LIST:
+                    markdown_output += self._format_list(component.content)
+                elif component.component_type == ContentType.IMAGE:
+                    markdown_output += self._format_image(component.content)
+                elif component.component_type == ContentType.EQUATION:
+                    markdown_output += f"$${component.content}$$ \n\n"
+                elif component.component_type == ContentType.FIGURECAPTION:
+                    markdown_output += f"{component.component_type} \n"
+                elif component.component_type == ContentType.TITLE:
+                    markdown_output += f"# {component.content}\n"
+                elif component.component_type == ContentType.TABLECAPTION:
+                    markdown_output += f"{component.content}\n"
+                elif component.component_type == ContentType.REFERENCE:
+                    markdown_output += f"{component.content}\n"
+                elif component.component_type == ContentType.CHART:
+                    markdown_output += self._format_chart(component.content)
+                elif component.component_type == ContentType.FOOTER and not ignore_footer:
+                    markdown_output += f"{component.content}\n"
+                    
+        return
+
+    def _format_table(self, table_data: str) -> str:
+        if not isinstance(table_data, list) or not all(isinstance(row, list) for row in table_data):
+            return "Error: Invalid table format" # Handle cases where table format is wrong
+        markdown_table = ""
+        for i, row in enumerate(table_data):
+            markdown_table += "| " + " | ".join(map(str, row)) + " |\n"
+            if i == 0:  # Add header separator
+                markdown_table += "| " + " | ".join(["---"] * len(row)) + " |\n"
+        return markdown_table
+    
+    def _format_list(self, list_data: str) -> str:
+        if not isinstance(list_data, list):
+            return "Error: Invalid list format"
+        markdown_list = ""
+        for item in list_data:
+            markdown_list += f"- {item}\n"
+        return markdown_list
+    
+    def _format_image(self, image_data) -> str:
+        try:
+            encoded_string = base64.b64encode(image_data).decode("utf-8")
+            return encoded_string # Assumes PNG change if needed
+        except Exception as e:
+            logging.error(f"Error embedding image: {e}")
+            return "Error displaying image.\n\n"
+ 
+    def _format_chart(self, chart_data) -> str:
+        try:
+            encoded_string = base64.b64encode(chart_data).decode("utf-8")
+            return encoded_string # Assumes PNG change if needed
+        except Exception as e:
+            logging.error(f"Error embedding image: {e}")
+            return "Error displaying image.\n\n"
